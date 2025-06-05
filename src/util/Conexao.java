8ce2aec125b658e966894fe0a4420efe602ac0f3
Fernando Perinotto <124933882+yFernand0W7@users.noreply.github.com>
2025-06-05 00:22:42 -0300

    Update Conexao.java

--- src/util/Conexao.java
+++ src/util/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
     private static final String URL = "YOU_DATABASE_URL";
     private static final String USUARIO = "YOU_DATABASE_USER";
-    private static final String SENHA = "#YOU_DATABASE_PASSWORD";
+    private static final String SENHA = "YOU_DATABASE_PASSWORD";
 
     public static Connection getConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
